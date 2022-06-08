a3d99525169535e733dbd3f663098e071f7fdda9
harishrockybhai <95605196+harishrockybhai@users.noreply.github.com>
2022-06-08 15:28:28 +0000

    problem09.py

--- ActivitySet01/problem04.py
+++ ActivitySet01/problem04.py
@@ -1,3 +1,11 @@
 # Conditional Execution
+hrs = float(input("Enter Hours:"))
+rate=float(input("enter the rate"))
+if(hrs>=40):
+    hrs1=hrs-40
+    Pay=(40*rate)+(hrs1*1.5*rate)
+    print(Pay)   
+else: 
+    Pay=hrs*rate
+    print(Pay)
 
-hrs = input("Enter hours? ")
--- ActivitySet01/problem09.py
+++ ActivitySet01/problem09.py
@@ -1,3 +1,15 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+
+fname=input("enter file name :")
+fh=open(fname)
+lst=list()
+for line in fh:
+    word=line.rstrip().strip()
+    for element in word:
+        if element in lst:
+            continue
+        else:
+            lst.append(element)
+lst.sort()
+print(lst)
